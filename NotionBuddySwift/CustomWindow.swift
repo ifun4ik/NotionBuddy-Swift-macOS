28a5c36d34aa0fc63bb85dea32c81b0192eafe94
Igor Alexandrov (Harry) <funnydemscreation@gmail.com>
2023-09-30 18:05:25 +0300

    Action bar from the lib

--- NotionBuddySwift/CustomWindow.swift
+++ NotionBuddySwift/CustomWindow.swift
@@ -1,11 +1,6 @@
-import SwiftUI
-
-class CustomWindow: NSWindow {
-    override var canBecomeKey: Bool {
-        return true
-    }
-
-    override var canBecomeMain: Bool {
-        return true
-    }
-}
+//import SwiftUI
+//
+//class CustomWindow: NSWindow {
+//    override var canBecomeKey: Bool { return true }
+//    override var canBecomeMain: Bool { return true }
+//}
--- NotionBuddySwift/GlobalShortcutManager.swift
+++ NotionBuddySwift/GlobalShortcutManager.swift
@@ -1,47 +1,25 @@
-
-import HotKey
-import Cocoa
 import SwiftUI
-
-import HotKey
 import Cocoa
-import SwiftUI
+import HotKey
+import DSFQuickActionBar
 
 class GlobalShortcutManager {
     static let shared = GlobalShortcutManager()
     var captureHotKey: HotKey?
-    var captureWindowController: CaptureWindowController?
+    var actionBar: DSFQuickActionBar?
 
     private init() {}
 
     func setupGlobalShortcut() {
         captureHotKey = HotKey(key: .n, modifiers: [.command, .control], keyDownHandler: {
-            self.showCaptureWindow()
+            self.showActionBar()
         })
     }
 
-    func showCaptureWindow() {
-        let captureView = CaptureView()
-        let window = CustomWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 200),
-            styleMask: [.borderless],
-            backing: .buffered,
-            defer: false
-        )
-
-        // Position the window on the screen where the mouse cursor is located
-        if let screen = NSScreen.screens.first(where: { NSMouseInRect(NSEvent.mouseLocation, $0.frame, false) }) {
-            let screenRect = screen.visibleFrame
-            let xPos = screenRect.origin.x + (screenRect.width - 480) / 2
-            let yPos = screenRect.origin.y + (screenRect.height - 200) / 2
-            window.setFrameOrigin(NSPoint(x: xPos, y: yPos))
+    func showActionBar() {
+        if actionBar == nil {
+            actionBar = DSFQuickActionBar()
         }
-
-        captureWindowController = CaptureWindowController(window: window)
-        captureWindowController?.setupEventMonitor()
-        window.contentView = NSHostingView(rootView: captureView)
-        window.makeKeyAndOrderFront(nil)
-        window.level = .floating
-        captureWindowController?.showWindow(nil)
+        actionBar?.present(parentWindow: nil, placeholderText: "Type something...", searchImage: NSImage(systemSymbolName: "square.and.pencil", accessibilityDescription: nil), initialSearchText: "")
     }
 }
