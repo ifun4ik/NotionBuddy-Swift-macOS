edff13c0866b6f737c4c6f1758967f7abb4096ff
Igor Alexandrov (Harry) <funnydemscreation@gmail.com>
2023-11-02 00:20:59 +0200

    Keyboard navigation works fine. Gotta fix the template selection

--- NotionBuddySwift/CaptureView.swift
+++ NotionBuddySwift/CaptureView.swift
@@ -16,12 +16,16 @@ struct CaptureView: View {
         }
     }
     
+    private var displayTemplates: [Template] {
+        return Array(filteredTemplates.prefix(5))
+    }
+    
     private var minHeight: CGFloat {
-        return filteredTemplates.isEmpty ? 48 + 8 * 4 : CGFloat(filteredTemplates.count > 5 ? 5 : filteredTemplates.count) * 48 + 8 * 4
+        return filteredTemplates.isEmpty ? 48 + 8 * 4 : CGFloat(displayTemplates.count) * 48 + 8 * 4
     }
     
     private var maxHeight: CGFloat {
-        return filteredTemplates.isEmpty ? 48 + 8 * 4 : CGFloat(filteredTemplates.count > 5 ? 5 : filteredTemplates.count) * 48 + 8 * 4
+        return filteredTemplates.isEmpty ? 48 + 8 * 4 : CGFloat(displayTemplates.count) * 48 + 8 * 4
     }
     
     var body: some View {
@@ -31,113 +35,123 @@ struct CaptureView: View {
                 Image(systemName: "command")
                     .resizable()
                     .frame(width: 16, height: 16)
-                    .foregroundColor(Color(red: 225/255, green: 238/255, blue: 246/255, opacity: 1))
+                    .foregroundColor(Constants.iconSecondary)
                 
                 TextField("Type something...", text: $capturedText)
                     .frame(height: 22)
                     .padding(.top, 2)
                     .textFieldStyle(.plain)
                     .font(
-                        Font.custom("Manrope", size: 16)
+                        Font.custom("SF Pro Text", size: 16)
                             .weight(.medium)
                     )
-                    .foregroundColor(Color(red: 0.88, green: 0.93, blue: 0.96))
+                    .foregroundColor(Constants.textPrimary)
                     .introspect(.textField, on: .macOS(.v10_15, .v11, .v12, .v13, .v14)) { textField in
                         DispatchQueue.main.asyncAfter(deadline: .now()) {
                             textField.becomeFirstResponder()
                             textField.currentEditor()?.selectedRange = NSRange(location: textField.stringValue.count, length: 0)
                         }
                     }
+                    .onAppear {
+                        setupKeyEventHandling()
+                    }
             }
             .padding(16)
-            .background(Color(#colorLiteral(red: 0.035, green: 0.063, blue: 0.101, alpha: 1)))
+            .background(Color.white)
             .cornerRadius(8)
             .overlay(
                 RoundedRectangle(cornerRadius: 8)
-                    .strokeBorder(Color(red: 0.88, green: 0.93, blue: 0.96).opacity(0.1), lineWidth: 1)
+                    .inset(by: 1)
+                    .stroke(Constants.bgPrimaryStroke, lineWidth: 1)
             )
             
             //MARK: Selection
             VStack (spacing: 0){
-                Group {
-                    Text("Pick the template")
-                        .font(Font.custom("Manrope", size: 16).weight(.semibold))
-                        .foregroundColor(Color(red: 0.88, green: 0.93, blue: 0.96))
-                        .frame(maxWidth: .infinity, alignment: .topLeading)
-                }
-                .padding(.horizontal, 16)
-                .padding(.top, 16)
-                .padding(.bottom, 8)
-                
-                if filteredTemplates.isEmpty {
+                if displayTemplates.isEmpty {
                     Text("Nothing found")
                         .font(Font.custom("Manrope", size: 16).weight(.semibold))
-                        .foregroundColor(Color(red: 0.88, green: 0.93, blue: 0.96))
+                        .foregroundColor(Constants.textPrimary)
                         .padding(.horizontal, 16)
                         .frame(maxWidth: .infinity, minHeight: 48, maxHeight: 48, alignment: .leading)
                 } else {
-                    ForEach(Array(filteredTemplates.prefix(5).enumerated()), id: \.element) { index, template in
+                    ForEach(Array(displayTemplates.enumerated()), id: \.element) { index, template in
                         HStack {
                             Text(template.name ?? "No template name")
-                                .font(Font.custom("Manrope", size: 16).weight(.semibold))
-                                .foregroundColor(Color(red: 0.88, green: 0.93, blue: 0.96))
+                                .font(Font.custom("Onest", size: 16).weight(.semibold))
+                                .foregroundColor(Constants.textPrimary)
                         }
                         .padding(.horizontal, 16)
                         .frame(maxWidth: .infinity, minHeight: 48, maxHeight: 48, alignment: .leading)
-                        .background(index == selectedIndex ? Color.blue.opacity(0.2) : Color.clear)  // Highlight if selected
+                        .background(index == selectedIndex ? Color.blue.opacity(0.2) : Color.clear)
                         .onTapGesture {
-                            selectedIndex = index  // Update selection on tap
+                            selectedIndex = index
                         }
                     }
                 }
             }
             .frame(minHeight: minHeight, maxHeight: maxHeight)
-            .background(Color(#colorLiteral(red: 0.035, green: 0.063, blue: 0.101, alpha: 1)))
+            .background(Color.white)
             .cornerRadius(8)
             .overlay(
                 RoundedRectangle(cornerRadius: 8)
-                    .strokeBorder(Color(red: 0.88, green: 0.93, blue: 0.96).opacity(0.1), lineWidth: 1)
+                    .inset(by: 1)
+                    .stroke(Constants.bgPrimaryStroke, lineWidth: 1)
             )
             
         }
-        .onReceive(NotificationCenter.default.publisher(for: NSApplication.didBecomeActiveNotification)) { _ in
-            NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
-                handleKeyEvent(event)
-                return event
-            }
+    }
+    
+    private func setupKeyEventHandling() {
+        NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
+            handleKeyEvent(event)
+            return event
         }
     }
     
     private func handleKeyEvent(_ event: NSEvent) {
-            guard !filteredTemplates.isEmpty else { return }
-            switch event.keyCode {
-            case 125:  // Down arrow key
-                selectedIndex = min((selectedIndex ?? -1) + 1, filteredTemplates.count - 1)
-            case 126:  // Up arrow key
-                selectedIndex = max((selectedIndex ?? 0) - 1, 0)
-            case 36:  // Enter/Return key
-                if let index = selectedIndex {
-                    let selectedTemplate = filteredTemplates[index]
-                    handleCommit(selectedTemplate)
-                }
-            default:
-                break
+        guard !displayTemplates.isEmpty else { return }
+        switch event.keyCode {
+        case 125:  // Down arrow key
+            if let currentIndex = selectedIndex, currentIndex < displayTemplates.count - 1 {
+                selectedIndex = currentIndex + 1
+            } else {
+                selectedIndex = 0
             }
+        case 126:  // Up arrow key
+            if let currentIndex = selectedIndex, currentIndex > 0 {
+                selectedIndex = currentIndex - 1
+            } else {
+                selectedIndex = displayTemplates.count - 1
+            }
+        case 36:  // Enter/Return key
+            if let index = selectedIndex, index < displayTemplates.count {
+                let selectedTemplate = displayTemplates[index]
+                handleCommit(selectedTemplate)
+            }
+        default:
+            break
         }
+    }
     
     private func handleCommit(_ selectedTemplate: Template? = nil) {
-            if let selectedTemplate = selectedTemplate {
-                print("Selected Template: \(selectedTemplate.name ?? "No name")")
-            } else {
-                print("Captured Text: \(capturedText)")
-            }
-            NotificationCenter.default.post(name: NSNotification.Name("CloseCaptureWindow"), object: nil)
+        if let selectedTemplate = selectedTemplate {
+            print("Selected Template: \(selectedTemplate.name ?? "No name")")
+        } else {
+            print("Captured Text: \(capturedText)")
         }
+        NotificationCenter.default.post(name: NSNotification.Name("CloseCaptureWindow"), object: nil)
+    }
 }
-
 
 struct CaptureView_Previews: PreviewProvider {
     static var previews: some View {
         CaptureView()
     }
+}
+
+struct Constants {
+    static let bgPrimary: Color = .white
+    static let bgPrimaryStroke: Color = Color(red: 0.91, green: 0.91, blue: 0.91)
+    static let iconSecondary: Color = Color(red: 0.62, green: 0.62, blue: 0.65)
+    static let textPrimary: Color = Color(red: 0.27, green: 0.29, blue: 0.38)
 }
